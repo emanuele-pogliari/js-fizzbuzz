c08b7e74ab7b5b9e89f15a97cbc176731bbd06ad
emanuele-pogliari <manu.pogliari@gmail.com>
2024-01-31 16:58:58 +0100

    js calculation completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 const gridElement = document.querySelector("#grid");
 
+for (let i = 1; i <= 100; i++) {
+    const newElement = document.createElement("div");
+    gridElement.append(newElement);
+    newElement.className = "square";
+    newElement.innerHTML = i;
+    console.log(i)
+    if (i % 3 === 0) {
+        newElement.innerText = "fizz";
+        newElement.className += " cass";
+        newElement.style.color = "blue";
+
+    }
+    else if (i % 5 === 0) {
+        newElement.innerText = "buzz";
+        newElement.style.color = "yellow";
+    }
+    if (i % 5 === 0 && i % 3 === 0) {
+        newElement.innerText = "s. francisco";
+        newElement.style.color = "red";
+    }
+}
